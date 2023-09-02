f33c83d61eb73be0b1be0f45145d25a91957fc27
Abdelwahab <ab28fb@gmail.com>
2023-09-02 21:30:06 +0100

    expo router

---
+++ app/_layout.jsx
@@ -0,0 +1,11 @@
+import { Slot } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function HomeLayout() {
+  return (
+    // <View>
+    //   <Text> Hello World</Text>
+    // </View>
+    <Slot />
+  );
+}
